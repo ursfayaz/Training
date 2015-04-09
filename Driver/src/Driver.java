8ef5c0204ce33c4fedf728d0bbfc0069620c6cc3
ursfayaz <ursfayaz@gmail.com>
2015-04-09 08:47:52 +0530

    Added a new class for Keywords

--- Driver/src/Driver.java
+++ Driver/src/Driver.java
@@ -3,11 +3,9 @@ import Fillo.*;
 import org.openqa.selenium.ie.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.*;
-
+public v_obj_id
 public class Driver {
-	public enum keyword {
-		lAUNCH_APP, LOGIN_APP, LOGOUT_APP, INPUT, RETRIEVE, COMPARE, ARITHMETIC_CALC, CALENDAR_ADD, ROUND_OFF, DATE_DIFFERENCE
-	}
+	
 	
 	/**
 	 * @param args
@@ -32,33 +30,7 @@ public class Driver {
 			String v_obj_class = RS.getField("Obj_Class").trim();
 			String v_keyword = RS.getField("Keyword").trim();
 			String v_Data = RS.getField("Data").trim();
-			keyword v_obj_keyword = keyword.valueOf(v_keyword);
-			switch (v_obj_keyword){
-			case lAUNCH_APP:
-				
-				System.out.println(v_Data);
-				driver.get(v_Data);
-				
-			case INPUT:
-				
-				WebElement TXT_name = driver.findElement(By.name(v_obj_id));
-				TXT_name.sendKeys(v_Data);
-				int j;
-				for (j=1;j<10;j++){
-					System.out.println("Iam the value : " + j);
-				}
-			case COMPARE:
-				WebElement LBL_id = driver.findElement(By.id(v_Data));
-				String v_Error_msg= LBL_id.getText();
-				if (v_Error_msg == v_Data){
-					System.out.println(v_Error_msg + " : Pass");				
-					
-				}
-				else
-				{
-					System.out.println(v_Error_msg + " : Fail");
-				}
-			}
+			
 			
 			
 		}
